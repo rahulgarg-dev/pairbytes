d180fc545d111f03b862ca7e0a273bbee4fba428
Rahul Garg <rahulaggarwal31090@gmail.com>
2026-01-31 14:33:16 +0530

    Improve UI and product quantity interaction

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,19 +1,3 @@
-// import React from 'react'
-// import ProductCard from "./ProductCard";
-
-// function ProductList({ products, addToCart }) {
-//   return (
-//     <div className="products">
-//       {products.map(p => (
-//         <ProductCard key={p.id} product={p} addToCart={addToCart} />
-//       ))}
-//     </div>
-//   );
-// }
-
-
-// export default ProductList;
-
 import React from "react";
 import ProductCard from "./ProductCard";
 
